ee7f92ce69ce55758476d4c01b79e683a2f7ed93
wangda <1950932406@qq.com>
2026-05-15 18:34:02 +0800

    添加架构指导.md

--- Logging.h
+++ Logging.h
@@ -1,10 +1,30 @@
-//
-// Created by adminstrator on 2026/5/15.
-//
+#pragma once
 
-#ifndef TINYLOGLIBRARY_LOGGING_H
-#define TINYLOGLIBRARY_LOGGING_H
+#include "Logger.h"
+#include <string>
 
-// 日志输入入口
+namespace logging {
 
-#endif //TINYLOGLIBRARY_LOGGING_H
+    // 获取默认 Logger（全局单例）
+    Logger* GetDefaultLogger();
+
+    // ---------- 函数式入口 ----------
+    // 第一版不引入 fmt，直接用 std::string
+
+    inline void Debug(const std::string& msg) {
+        GetDefaultLogger()->Log(LogLevel::Debug, msg);
+    }
+    inline void Info(const std::string& msg) {
+        GetDefaultLogger()->Log(LogLevel::Info, msg);
+    }
+    inline void Warn(const std::string& msg) {
+        GetDefaultLogger()->Log(LogLevel::Warn, msg);
+    }
+    inline void Error(const std::string& msg) {
+        GetDefaultLogger()->Log(LogLevel::Error, msg);
+    }
+    inline void Critical(const std::string& msg) {
+        GetDefaultLogger()->Log(LogLevel::Critical, msg);
+    }
+
+} // namespace logging
